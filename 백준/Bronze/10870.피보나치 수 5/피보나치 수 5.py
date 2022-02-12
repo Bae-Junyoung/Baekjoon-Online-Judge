d9638a1b5e326a6d17802c9015f12dfea3cfa11e
Bae-Junyoung <66288942+Bae-Junyoung@users.noreply.github.com>
2022-02-12 13:07:25 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/10870.피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870.피보나치 수 5/피보나치 수 5.py
@@ -1,10 +1,8 @@
 N = int(input())
 
 def fib(N):
-    if N==0:
-        return 0
-    elif N==1:
-        return 1
+    if N<=1:
+        return N
     else:
         return fib(N-1) + fib(N-2)
     
